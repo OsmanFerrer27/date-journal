629a7af97ee3d2772afc92d91da58a3b09622efd
diego <rincondiego2002@gmail.com>
2025-01-26 22:35:38 -0400

    removed username field in signup

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -11,10 +11,6 @@ export default function Signup() {
             <input type="text" id="email" name="email" className="mt-1 text-gray-700 block w-full h-8 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
           </div>
           <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username:</label>
-            <input type="text" id="username" name="username" className="mt-1 text-gray-700 block w-full h-8 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-          </div>
-          <div>
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
             <input type="password" id="password" name="password" className="mt-1 text-gray-700 block w-full h-8 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
           </div>
